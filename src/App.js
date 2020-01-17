1d9d6e7ebbcecea72b3dfb994710a3cf73177505
adityapramudit8 <adityapramudita23@gmail.com>
2020-01-17 15:12:29 +0700

    add forms and style cart

--- src/App.js
+++ src/App.js
@@ -33,41 +33,41 @@ const products = [
     name: "Table",
     description: "Office Property",
     img: "https://www.klikoffice.co.id/image/cache/data/meja-kantor/meja-kantor-lunar-1-2-biro-1-laci-1-pintu-208-700x700.jpg",
-    price: 100
+    price: 135000
   },
 
 
   {
     id: 3,
-    name: "",
-    description: "Fabelio",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
+    name: "TV Samsung",
+    description: "Samsung",
+    img: "https://www.westcoasthifi.com.au/wp-content/uploads/2016/09/samsung-50ku7000-50-inch-4k-uhd-smart-led-tv-500x500.jpg",
+    price: 500000
   },
 
   {
     id: 4,
-    name: "Banana",
-    description: "I need those carbs",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
+    name: "Gaming Chair",
+    description: "Razer",
+    img: "http://kkomputer.com/1813-large_default/harga-jual-spesifikasi-review-gaming-chair-kursi-game-marvo-scorpion-ch101-malang.jpg",
+    price: 450000
   },
 
 
   {
     id: 5,
-    name: "Orange",
-    description: "Orange are orange",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
+    name: "iPhone 11",
+    description: "iPhone",
+    img: "https://media.dinomarket.com/docs/imgTD/2019-12/pic_iPhone11_Black1_051219191236_ll.jpg.jpg",
+    price: 3000000
   },
 
   {
     id: 6,
-    name: "Orange",
-    description: "Orange are orange",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
+    name: "Apple Watch",
+    description: "Apple",
+    img: "https://media.dinomarket.com/docs/imgTD/2017-12/pic_apple_watch_grey1_211217211235_ll.jpg.jpg",
+    price: 5000000
   }
 
 ];
@@ -131,11 +131,14 @@ class App extends Component {
                         products.map(p => <Product key={p.id} {...p} addFunc={this.handleAddFunc.bind(this)} />)
                       }
                     </>
-                    <ul>
-                    {
-                      this.state.cart.map(c => <li>{c.name} | units {c.units}</li>)
-                    }
-                    </ul>
+                    <div className="box">
+                      <h2>Cart List Item</h2>
+                      <ul>
+                      {
+                        this.state.cart.map(c => <li>{c.name} | Units {c.units}</li>)
+                      }
+                      </ul>
+                    </div>
                   </main>
               </Route>
               <Route exact path='/admin'>
--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import '../App.css';
 import { Control, Form } from 'react-redux-form';
 
 class MyForm extends React.Component {
-  handleSubmit(val) {
-    // Do anything you want with the form value
-    console.log(val);
+  handleSubmit(val) { 
+    console.log(val)
   }
 
   render() {
     return (
-      <Form model="user" onSubmit={(val) => this.handleSubmit(val)}>
+      <>
+      <h1>Tambah Barang Dipay</h1>
+      <Form className="form-style" model="user" onSubmit={(val) => this.handleSubmit(val)}>
         <label>Nama Barang</label><br/>
-        <Control.text model=".nama" /><br/>
+        <Control.text className="input-style" model=".nama" /><br/>
         <label>Deskripsi</label><br/>
-        <Control.text model=".deskripsi" /><br/>
+        <Control.text className="input-style" model=".deskripsi" /><br/>
         <label>Gambar</label><br/>
-        <Control.text model=".gambar" /><br/>
+        <Control.text className="input-style" model=".gambar" /><br/>
         <label>Harga</label><br/>
-        <Control.text model=".harga" /><br/>
-        <button>Submited</button>
+        <Control.text className="input-style" model=".harga" /><br/>
+        <br/>
+        <button className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn">Submited</button>
       </Form>
+      </>
     );
   }
 }
